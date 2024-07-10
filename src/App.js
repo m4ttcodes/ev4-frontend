2af7f8f151f7976c4de4adda47d547c229aab9a7
m4ttcodes <vicenteaburtog@gmail.com>
2024-07-10 00:12:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TopBar from "./ui/TopBar";
+import Home from './ui/Home';
+import ListaClientes from './clientes/ListaClientes';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+      <div>
+        <TopBar/>
+      </div>
+
+    </Router>
+    
   );
 }
 
